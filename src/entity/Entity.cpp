d3a5b1b1c456ec30df7afd273f97b61551cc4fc4
Bouvet Emma <emma.bouvet.35@outlook.fr>
2025-05-26 16:56:30 +0200

    entity grid

--- src/entity/Entity.cpp
+++ src/entity/Entity.cpp
@@ -2,19 +2,27 @@
 #include <entity/Entity.h>
 #include <graphics/Renderer.h>
 
-Entity::Entity(const Vector2 &position, const std::string &name, const unsigned int layer)
-    : m_position(position), m_name(name), m_layer(layer)
+Entity::Entity(const Vector2Grid &position, const std::string &name, const unsigned int layer)
+    : m_grid(position), m_name(name), m_layer(layer), m_absolute(0,0)
 {
 }
 
-void Entity::setPosition(const Vector2 &position)
+void Entity::setPosition(const Vector2Grid &position)
 {
-    m_position = position;
+    m_grid = position;
 }
 
-const Vector2 &Entity::getPosition() const
+void Entity::setAbsolute(const Vector2 &position) const{
+    m_absolute = position;
+}
+
+const Vector2Grid &Entity::getPosition() const
 {
     return m_position;
+}
+
+const Vector2 &Entity::getAbsolute() const{
+    return m_absolute;
 }
 
 unsigned int Entity::getLayer() const
