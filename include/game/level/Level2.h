a4667327eca3b061531f72fd945642074c1b4423
niels <niels.crolais@gmail.com>
2025-05-27 12:57:12 +0200

    bah ça fonctionne emma, euuuuh, je sais pas, euuuh, j'ai pas touché, heuuuu

--- include/game/level/Level2.h
+++ include/game/level/Level2.h
@@ -11,11 +11,12 @@ class Level2 : public LevelBase
     Level2()
         : LevelBase(std::unique_ptr<PlayerWord>(new PlayerWord(Vector2F(1.5, 1.5) * Config::tile)),
                     std::unique_ptr<Camera>(new Camera(Vector2F(0, 0))),
-                    std::unique_ptr<Map>(new Map(Vector2F(0, 0), "map", Reader::loadData("assets/map/map_2.csv"))),std::unique_ptr<EndMap>(new EndMap(Vector2F(39,43))))
+                    std::unique_ptr<Map>(new Map(Vector2F(0, 0), "map", Reader::loadData("assets/map/map_2.csv"))),
+                    std::unique_ptr<EndMap>(nullptr))
     {
-      //Vector2F fin =m_map->getTaille();
-      //Vector2F fin_valide = Vector2F(fin[0]-2,fin[1]-2);
-      //m_endmap= std::unique_ptr<EndMap>(new EndMap(fin_valide));
+        Vector2F fin = m_map->getTaille();
+        Vector2F fin_valide = Vector2F(fin[0] - 2, fin[1] - 2);
+        m_endmap = std::unique_ptr<EndMap>(new EndMap(fin_valide));
     }
 
     std::unique_ptr<LevelBase> getNextLevel() override
